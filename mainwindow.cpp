cd19952bac663f7855672a6895132afd0a9e0dc4
Friedrich Horschig <f.horschig@googlemail.com>
2012-09-27 20:54:16 +0200

    First layout. Not that detailed as there is no access for me to the old MLP 2000 ... -.-

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,8 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_actionBeenden_triggered()
+{
+    close();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -10,11 +10,14 @@ class MainWindow;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-    
+
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-    
+
+private slots:
+    void on_actionBeenden_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
